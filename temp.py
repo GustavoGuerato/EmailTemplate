8a27c6817cc2ba92e4657a1208fa7022fac96286
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-17 15:48:07 -0300

    some bug fixes

--- temp.py
+++ temp.py
@@ -1,7 +1,21 @@
 from string import Template
 from datetime import datetime
+import os
 
-with open('template.html', 'r') as html:
-    template = Template(html.read())
-    dia = datetime.now().strftime('%d/%m/%Y')
-    corpo_msg = template.substitute(nome='Gustavo Guerato', data=dia)
+try:
+
+    diretorio_script = os.path.dirname(os.path.realpath(__file__))
+
+    caminho_template = os.path.join(diretorio_script, 'email.html')
+
+    if not os.path.exists(caminho_template):
+        raise FileNotFoundError(f"Erro: O arquivo 'email.html' não foi encontrado em '{diretorio_script}'")
+
+    with open(caminho_template, 'r') as html:
+        template = Template(html.read())
+        data_atual = datetime.now().strftime('%d/%m/%Y')
+        corpo_msg = template.substitute(nome='Gustavo Guerato', data=data_atual)
+        print("Mensagem gerada com sucesso:", corpo_msg)
+
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
